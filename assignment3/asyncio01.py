b7243e3bfeebe5f80cd186e36d39c0a501480f2e
NungMark <120709803+NungMark@users.noreply.github.com>
2024-06-26 11:25:55 +0700

    SubmitProcess

--- assignment3/asyncio01.py
+++ assignment3/asyncio01.py
@@ -1,1 +1,11 @@
 # check the type of a coroutine
+import asyncio
+# define a coroutime
+async def custom_coro():
+    # await another coroutine
+    await asyncio.sleep(1)
+    
+    # create the coroutine
+    coro = custom_coro()
+    # check the type of t he coroutine
+    print(type(coro))
